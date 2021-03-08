26303789cdf155eea380efb39bca2c4ff258df2d
RLatva <55876966+RLatva@users.noreply.github.com>
2021-03-08 21:58:23 +0200

    Add files via upload

--- lab2/mycrypt.py
+++ lab2/mycrypt.py
@@ -8,17 +8,23 @@ def encode(s):
     digitmapping = dict(zip('1234567890!"#€%&/()=','!"#€%&/()=1234567890'))
     if len(s) > 1000:
         raise ValueError
+    s = s.ljust(1000, "c")
     for c in s:
-        if c.isalpha():
+        if c in ["å","ä","ö"]:
+            raise ValueError
+        elif c.isalpha():
             if c.islower():
                 c=c.upper()
+            elif c.isupper():
+                c=c.lower()
             # Rot13 the character for maximum security
             crypted+=codecs.encode(c,'rot13')
         elif c in digitmapping:
           crypted+=digitmapping[c]
-
-    return crypted
+        else:
+            raise ValueError
+    return crypted[:origlen]
 
 def decode(s):
-    return s
+    return encode(s)
 
--- lab2/test_mycrypt.py
+++ lab2/test_mycrypt.py
@@ -21,7 +21,7 @@ import mycrypt
     ("b", "O"),
     ("abc", "NOP"),
     ("abc123", 'NOP!"#'),
-    ("4", u'€')
+    ("4", '€')
 ])
 def test_encode(test_input, expected):
     '''Verify that strings given above match the expected results'''
@@ -32,17 +32,17 @@ def test_encode(test_input, expected):
     '123', '!"#','abc'])
 def test_encode_decode(test_input):
     '''Verify that decoding an encoded string returns original string'''
-    assert(mycrypt.decode(mycrypt.encode(test_input))) == test_input
+    assert(mycrypt.decode(mycrypt.encode((test_input)))) == test_input
 
 
-@pytest.mark.parametrize("invalid_input", ['+','åäö'])
+@pytest.mark.parametrize("invalid_input", ['+','åäö',"a"*1001])
 def test_invalid_char(invalid_input):
     '''Invalid characters should result in ValueError'''
     with pytest.raises(ValueError):
         mycrypt.encode(invalid_input)
 
 
-@pytest.mark.parametrize("invalid_input", [])
+@pytest.mark.parametrize("invalid_input", [7])
 def test_invalid_types(invalid_input):
     '''Invalid parameter types should raise TypeError'''
     with pytest.raises(TypeError):
@@ -63,3 +63,4 @@ def test_timing():
     timing2 = min(timeit.repeat('mycrypt.encode("a"*1000)',
                                 'import mycrypt', repeat=3, number=30))
     assert 0.95 * timing2 < timing1 < 1.05 * timing2
+
